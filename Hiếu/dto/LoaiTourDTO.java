4f42b1291fcd1ef2023dc1e5d0f4ec6154629038
Tran Viet Hieu - Hiếu <trhi4100@gmail.com>
2026-02-27 22:24:03 +0700

    Create LoaiTourDTO

---
+++ Hiếu/dto/LoaiTourDTO.java
@@ -0,0 +1,37 @@
+package org.example.dto;
+
+public class LoaiTourDTO {
+    private String maLoaiTour;
+    private String theLoai;
+
+    public LoaiTourDTO(){
+        this.maLoaiTour = "";
+        this.theLoai = "";
+    }
+
+    public LoaiTourDTO(String maLoaiTour, String theLoai) {
+        this.maLoaiTour = maLoaiTour;
+        this.theLoai = theLoai;
+    }
+
+    public void setMaLoaiTour(String maLoaiTour) {
+        this.maLoaiTour = maLoaiTour;
+    }
+
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
+
+    public String getMaLoaiTour() {
+        return maLoaiTour;
+    }
+
+    public String getTheLoai() {
+        return theLoai;
+    }
+
+    @Override
+    public String toString(){
+        return maLoaiTour + " - " + theLoai;
+    }
+}
