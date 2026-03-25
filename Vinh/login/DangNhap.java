ab3e157a56d75882d45eaa3f280a1e580d972ca8
Tran Viet Hieu - Hiếu <trhi4100@gmail.com>
2026-03-25 20:57:14 +0700

    Update DangNhap.java

--- Vinh/login/DangNhap.java
+++ Vinh/login/DangNhap.java
@@ -1,140 +1,149 @@
-
 package org.example.login;
 
 import org.example.dao.TaiKhoanDAO;
-import org.example.dao._MyConnection;
 import org.example.dto.TaiKhoanDTO;
 import org.example.gui._MainFrame;
 
-import java.beans.Statement;
-import java.sql.Connection;
+import java.awt.*;
 import java.util.Arrays;
-import java.util.logging.Logger;
 import javax.swing.*;
 
 public class DangNhap extends JFrame {
-
-    private static final Logger logger = Logger.getLogger(DangNhap.class.getName());
-
     public DangNhap() {
         initComponents();
         setLocationRelativeTo(null);
     }
-    Connection c = _MyConnection.getConnection();
-    Statement st = null;
+
     private final TaiKhoanDAO taiKhoanDAO = new TaiKhoanDAO();
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JPasswordField();
-        jTextField2.setEchoChar('*');
+        jlbLogin = new JLabel();
+        jlbAccount = new JLabel();
+        jlbPassword = new JLabel();
+        loginBtn = new JButton();
+        logoutBtn = new JButton();
+        txtUsername = new JTextField();
+        txtPassword = new JPasswordField();
+        txtPassword.setEchoChar('*');
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jLabel1.setText("Đăng nhập");
+        jlbLogin.setFont(new Font("Segoe UI", 1, 24)); // NOI18N
+        jlbLogin.setText("Đăng nhập");
 
-        jLabel2.setText("Tài khoản:");
+        jlbAccount.setText("Tài khoản:");
 
-        jLabel3.setText("Mật khẩu:");
+        jlbPassword.setText("Mật khẩu:");
 
-        jButton1.setText("Đăng nhập");
-        jButton1.addActionListener(this::jButton1ActionPerformed);
+        // define login and logout function
+        login();
+        logout();
 
-        jButton2.setText("Thoát");
-        jButton2.addActionListener(this::jButton2ActionPerformed);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGap(136, 136, 136)
-                                                .addComponent(jLabel1))
-                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                .addComponent(jlbLogin))
+                                        .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                                 .addGap(106, 106, 106)
-                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                                        .addComponent(jLabel2)
-                                                        .addComponent(jLabel3))
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                                        .addComponent(jTextField1)
-                                                        .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))))
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                                        .addComponent(jlbAccount)
+                                                        .addComponent(jlbPassword))
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                                        .addComponent(txtUsername)
+                                                        .addComponent(txtPassword, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))))
+                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(82, 82, 82)
-                                .addComponent(jButton1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
-                                .addComponent(jButton2)
+                                .addComponent(loginBtn)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                                .addComponent(logoutBtn)
                                 .addGap(80, 80, 80))
         );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jLabel1)
+                                .addComponent(jlbLogin)
                                 .addGap(50, 50, 50)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jLabel2)
-                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jlbAccount)
+                                        .addComponent(txtUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                 .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jLabel3)
-                                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jButton1)
-                                        .addComponent(jButton2))
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jlbPassword)
+                                        .addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(loginBtn)
+                                        .addComponent(logoutBtn))
                                 .addGap(64, 64, 64))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        String username = jTextField1.getText().trim();
-        char[] passwordChars = jTextField2.getPassword();
-        String password = new String(passwordChars).trim();
-        Arrays.fill(passwordChars, '\0');
+    private void login(){
+        loginBtn = createBtn("Đăng nhập", Color.GREEN);
+        loginBtn.addActionListener(v -> {
+            String username = txtUsername.getText().trim();
+            char[] passwordChars = txtPassword.getPassword();
+            String password = new String(passwordChars).trim();
+            Arrays.fill(passwordChars, '\0');
 
-        if (username.isEmpty() || password.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Vui lòng nhập đầy đủ tài khoản và mật khẩu.");
-            return;
-        }
+            if (username.isEmpty() || password.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Vui lòng nhập đầy đủ tài khoản và mật khẩu.");
+                return;
+            }
 
-        TaiKhoanDTO taiKhoan = taiKhoanDAO.dangNhap(username, password);
-        if (taiKhoan == null) {
-            JOptionPane.showMessageDialog(this, "Sai tài khoản hoặc mật khẩu.");
-            return;
-        }
+            TaiKhoanDTO taiKhoan = taiKhoanDAO.dangNhap(username, password);
+            if (taiKhoan == null) {
+                JOptionPane.showMessageDialog(this, "Sai tài khoản hoặc mật khẩu.");
+                return;
+            }
 
-        PhanQuyen.dangNhap(taiKhoan);
-        _MainFrame mainFrame = new _MainFrame(taiKhoan);
-        mainFrame.setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+            PhanQuyen.dangNhap(taiKhoan);
+            _MainFrame mainFrame = new _MainFrame(taiKhoan);
+            mainFrame.setVisible(true);
+            this.dispose();
+        });
+    }
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void logout(){
+        logoutBtn = createBtn("Thoát", Color.RED);
+        logoutBtn.addActionListener(v -> {
+            dispose();
+        });
+    }
+
+    private JButton createBtn(String text, Color color){
+        JButton btn = new JButton(text);
+        btn.setBackground(color);
+        btn.setForeground(Color.WHITE);
+        btn.setFocusPainted(false);
+        btn.setFont(new Font("SansSerif", Font.BOLD, 13));
+        btn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // in south panel
+
+        btn.setContentAreaFilled(true);
+        btn.setOpaque(true);
+        btn.setBorderPainted(false);
+        return btn;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JPasswordField jTextField2;
-    // End of variables declaration//GEN-END:variables
+    private JButton loginBtn;
+    private JButton logoutBtn;
+    private JLabel jlbLogin;
+    private JLabel jlbAccount;
+    private JLabel jlbPassword;
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
 }
